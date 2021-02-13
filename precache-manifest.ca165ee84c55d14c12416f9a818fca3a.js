bec5d734e56409028bac5686ee17dcb1beeb326d
Nathan Griffiths <ngriffiths@postmedia.com>
2021-02-12 17:58:47 -0800

    Updates

---
+++ precache-manifest.ca165ee84c55d14c12416f9a818fca3a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "605ceeab1120fcfaa37fcf598e4f16f0",
+    "url": "/covid-national-ha-map/index.html"
+  },
+  {
+    "revision": "ea025daa3a2904a331e8",
+    "url": "/covid-national-ha-map/static/css/2.7b0cfacc.chunk.css"
+  },
+  {
+    "revision": "e0ef3406573981c8f752",
+    "url": "/covid-national-ha-map/static/css/main.d891ea99.chunk.css"
+  },
+  {
+    "revision": "ea025daa3a2904a331e8",
+    "url": "/covid-national-ha-map/static/js/2.e70d3f0d.chunk.js"
+  },
+  {
+    "revision": "112b9e655310c562d28b7db550db74b9",
+    "url": "/covid-national-ha-map/static/js/2.e70d3f0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0ef3406573981c8f752",
+    "url": "/covid-national-ha-map/static/js/main.c7c9a4a1.chunk.js"
+  },
+  {
+    "revision": "2ad728153788ab563b67",
+    "url": "/covid-national-ha-map/static/js/runtime-main.55ad6646.js"
+  },
+  {
+    "revision": "6f6753fd733a216b24786a168657540a",
+    "url": "/covid-national-ha-map/static/media/BentonSansCond-Bold.6f6753fd.otf"
+  },
+  {
+    "revision": "a6640d8951e3d6c523b2b58b0db9e351",
+    "url": "/covid-national-ha-map/static/media/BentonSansCond-BoldItalic.a6640d89.otf"
+  },
+  {
+    "revision": "69a4217c7da7462a2511b1a8ecaefecf",
+    "url": "/covid-national-ha-map/static/media/BentonSansCond-RegItalic.69a4217c.otf"
+  },
+  {
+    "revision": "43cd4ac75abf53e257d21366d3322b0e",
+    "url": "/covid-national-ha-map/static/media/BentonSansCond-Regular.43cd4ac7.otf"
+  },
+  {
+    "revision": "cdd38a860699d029f95e76f21e51e216",
+    "url": "/covid-national-ha-map/static/media/Shift-Bold.cdd38a86.otf"
+  },
+  {
+    "revision": "5c3edbfdeedc59bb35fac3bf1a461926",
+    "url": "/covid-national-ha-map/static/media/Shift-BoldItalic.5c3edbfd.otf"
+  }
+]);
